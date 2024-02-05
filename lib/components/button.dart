56619d9cb5baa690b753eec19228a8aa0bd977ef
dohun0310 <dohun0310@naver.com>
2024-02-06 01:38:23 +0900

    edit buttons styling

--- lib/components/button.dart
+++ lib/components/button.dart
@@ -1,18 +1,38 @@
 import 'package:flutter/material.dart';
 
+import 'package:noisyalarm/theme.dart';
+
 class Button extends StatelessWidget {
   const Button({
     super.key,
+    required this.color,
+    required this.onTap,
     required this.text,
   });
 
+  final Color? color;
+  final void Function() onTap;
   final String text;
 
   @override
   Widget build(BuildContext context) {
-    return InkWell(
-      onTap: () {},
-      child: Text(text),
+    return Material(
+      color: color,
+      borderRadius: BorderRadius.circular(12),
+      child: InkWell(
+        onTap: onTap,
+        borderRadius: BorderRadius.circular(12),
+        child: Container(
+          padding: const EdgeInsets.all(8),
+          child: Text(
+            text,
+            style: ThemeTexts.bodyRegular.copyWith(
+              color: Theme.of(context).extension<AppExtension>()?.colors.text,
+            ),
+            textAlign: TextAlign.center,
+          ),
+        ),
+      ),
     );
   }
 }
